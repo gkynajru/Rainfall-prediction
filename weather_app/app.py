2e97e11901a7a78262bbe7ebcba3ab29e01c48c9
gkynajru <ntgiaky@gmail.com>
2024-12-13 11:09:53 +0700

    add folder weather_app

---
+++ weather_app/app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request
+<<<<<<< HEAD
+from utils.data_generator import fetch_weather_data
+import paho.mqtt.client as mqtt
+import threading
+import json
+from config import BROKER, PORT, TOPIC, TOKEN
+import pandas as pd
+import ssl
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+global location_temp
+global data
+
+location_temp = "Ky Thuong"
+data = fetch_weather_data(location_temp)
+new_df = data
+
+t = datetime.now()
+if t.minute != 0:
+    t = t.replace(minute=0, second=0, microsecond=0)
+t_1h = (t + timedelta(hours = 1)).strftime('%Y-%m-%dT%H:%M')
+t_2h = (t + timedelta(hours = 2)).strftime('%Y-%m-%dT%H:%M')
+t_3h = (t + timedelta(hours = 3)).strftime('%Y-%m-%dT%H:%M')
+
+new_df_filtered = new_df[new_df["date"].isin([t_1h, t_2h, t_3h])]["rain"]
+msg = ', '.join(new_df_filtered.astype(str))
+
+rain_data = {}
+
+def on_connect(client, userdata, flags, rc):
+    print('Connect with result code ' + str(rc))
+    if rc == 0:
+        client.subscribe(TOPIC)
+    else:
+        print(f"Failed to connect, return code {rc}")
+
+def on_message(client, userdata, message):
+    print(f"Message received: {message.payload.decode()}")
+
+def publish_message(client, msg):
+    try:
+        client.publish(TOPIC, msg)
+        print(f"Message sent: {msg}")
+    except Exception as e:
+        print(f"Error publishing message: {e}")
+
+def create_mqtt_client():
+    client = mqtt.Client()
+    client.username_pw_set(TOKEN, "") 
+    client.tls_set_context(ssl.create_default_context())
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.publish_message = publish_message
+    client.connect(BROKER, PORT, 60)
+    threading.Thread(target=client.loop_forever, daemon=True).start()
+    return client
+
+# Index route to display the rainfall graph
+@app.route("/")
+def index():
+    location = request.args.get("location", location_temp)
+    
+    client = create_mqtt_client()
+    client.publish_message(client, msg)
+
+    df = pd.DataFrame(data)
+    df["date"] = pd.to_datetime(df["date"])
+    json_str = df.to_json(orient='records', date_format='iso')
+    return render_template("index.html", data=json_str, location=location, locations=rain_data.keys())
+
+if __name__ == "__main__":
+    # # Start MQTT client in a separate thread
+    # create_mqtt_client()
+=======
+from utils.mqtt_client import start_mqtt_client, rain_data
+from utils.data_crawler import fetch_weather_data
+import pandas as pd
+
+app = Flask(__name__)
+
+# Index route to display the rainfall graph
+@app.route("/")
+def index():
+    location = request.args.get("location", "Ky Thuong")
+    data = rain_data.get(location, [])
+    return render_template("index.html", data=data, location=location, locations=rain_data.keys())
+
+if __name__ == "__main__":
+    # Start MQTT client in a separate thread
+    start_mqtt_client()
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+    # Start Flask app
+    app.run(debug=True)
---
+++ weather_app/config.py
@@ -0,0 +1,117 @@
+# MQTT Configuration
+<<<<<<< HEAD
+BROKER = "mqtt.flespi.io"
+PORT = 8883
+TOPIC = "topic/rain"
+TOKEN = "FlespiToken lCG8yJPUWRc9awe3M2AaTuKcqd5N4Nvgd1cByPklwkiuGmogcOgW6QWmURXOujSx"
+
+# Open-Meteo API Parameters
+PARAMS = {
+=======
+BROKER = "mqtt.eclipse.org"
+PORT = 1883
+TOPIC = "weather/rain_data"
+
+# Open-Meteo API Parameters
+LOCATIONS = {
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+    "Ky Thuong":
+        {
+            "latitude": 18.0316,
+            "longitude": 106.109,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1
+        },
+    "CC Thuy_loi":
+        {
+            "latitude": 18.312828,
+            "longitude": 105.9082,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1          
+        },
+    "My Loc":
+        {
+            "latitude": 18.383127,
+            "longitude": 105.747795,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1                  
+        },
+    "Van Trach":
+        {
+            "latitude": 17.60984,
+            "longitude": 106.450676,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1               
+        },
+    "Truong Xuan":
+        {
+            "latitude": 17.2751871,
+            "longitude": 106.6334756,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1
+        },
+    "Ho Trooc Trau":
+        {
+            "latitude": 17.398945,
+            "longitude": 106.57666,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1
+        },
+    "Cua Tung":
+        {
+            "latitude": 17.047451,
+            "longitude": 107.10173,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1
+        },
+    "Huong Linh":
+        {
+            "latitude": 16.695957,
+            "longitude": 106.762184,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1 
+        },
+    "Trieu Ai":
+        {
+            "latitude": 16.766256,
+            "longitude": 107.12237,
+            "hourly": "rain",
+<<<<<<< HEAD
+            "past_days": 1,
+=======
+>>>>>>> 41ae0e9768cd6fd491a93d03e616b885b5bfd844
+            "forecast_days": 1
+        }       
+}
