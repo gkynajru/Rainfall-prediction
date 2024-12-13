b0a6aed76d883f5208378ea95e6a817484486528
gkynajru <ntgiaky@gmail.com>
2024-12-13 11:09:31 +0700

    nothing

--- weather_app/utils/plot_helper.py
+++
@@ -1,6 +0,0 @@
-#If more complex plotting logic is required, add helper functions here.
-
-def prepare_plot_data(data):
-    times = [entry["time"] for entry in data]
-    rain_values = [entry["rain"] for entry in data]
-    return times, rain_values
